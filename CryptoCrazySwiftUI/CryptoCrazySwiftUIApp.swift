bbfa6f6bbc9942ef12dd81466fb170884c291548
Mark Santoro <mark_santoro@comcast.net>
2024-08-28 17:38:39 -0700

    Initial Commit

---
+++ CryptoCrazySwiftUI/CryptoCrazySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCrazySwiftUIApp.swift
+//  CryptoCrazySwiftUI
+//
+//  Created by Mark Santoro on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCrazySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
